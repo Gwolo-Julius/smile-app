f9f32a4b0c417c3ba74799e129675f4eea137eb0
Julius <gwolojulius2@gmail.com>
2024-11-27 12:25:22 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hello smile app from App jsx</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium odio eaque, esse fugiat optio iste obcaecati ab blanditiis tenetur quasi explicabo veritatis porro. Tempore laborum ad placeat cum facilis inventore.</p>
+    </>
+  );
+}
+
+export default App;
